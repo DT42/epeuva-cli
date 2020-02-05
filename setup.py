24d95bf7aa7cf244987c1fb2c5291213b18923c8
James Chien <shchien@dt42.io>
2020-02-05 11:10:06 +0800

    v0.1.1 release
    - Separate login creation function call from other API calls for better function coupling
    
    - Add unit test for API calls: core, apps and bases
    
    - Meet PEP8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='epeuva_cli',
-    version='0.1.0',
+    version='0.1.1',
     description='Epeuva CLI tool',
     url='https://github.com/DT42/epeuva-cli',
     author='DT42',
